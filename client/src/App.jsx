9fe1ebbd391eedb001b0ccc67215c2f0068f0ebd
github_Ukapromise <ukapromiseimo@gmail.com>
2022-04-30 01:57:40 +0100

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
